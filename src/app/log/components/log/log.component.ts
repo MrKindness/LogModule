4a326cca93f292a6479a72b175f02439b61d4e2f
MrKindness <vadim.tripadush.bz@gmail.com>
2021-05-30 15:36:13 +0300

    first commit

---
+++ src/app/log/components/log/log.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'log-component',
+  templateUrl: './log.component.html',
+  styleUrls: ['./log.component.scss'],
+})
+export class LogComponent {}
---
+++ src/app/log/log.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { LogComponent } from './components/log/log.component';
+
+@NgModule({
+  declarations: [LogComponent],
+  imports: [],
+  providers: [],
+})
+export class LogModule {}
