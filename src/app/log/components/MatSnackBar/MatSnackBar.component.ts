a52138db269e2134424dd9123b205e3da369cc74
MrKindness <vadim.tripadush.bz@gmail.com>
2021-06-03 00:21:08 +0300

    snackbar and new project structure

--- src/app/log/components/MatSnackBar/MatSnackBar.component.ts
+++ src/app/log/components/MatSnackBar/MatSnackBar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { LogService } from '../../services/LogService';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-mat-snack-bar',
   templateUrl: './MatSnackBar.component.html',
   styleUrls: ['./MatSnackBar.component.scss'],
 })
-
 export class MatSnackBarComponent {
-  constructor(private EventService: LogService){}
+  constructor(
+    private EventService: LogService,
+    private SnackBar: MatSnackBar
+  ) {}
+
+  duration = 5;
+
+  openSnackBar() {
+    this.SnackBar.open('Новое уведомление!', );
+  }
 }
